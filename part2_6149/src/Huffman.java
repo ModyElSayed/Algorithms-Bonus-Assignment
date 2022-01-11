475dfc5ecd7428f6d34f616e208d9e6600fd2955
mody <mody.elsayed555@icloud.com>
2022-01-11 17:21:18 +0200

    last edit for compression

--- part2_6149/src/Huffman.java
+++ part2_6149/src/Huffman.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -5,49 +9,109 @@ import java.util.PriorityQueue;
 public class Huffman {
     private final PriorityQueue<FrequencyNode> huffmanQueue;
     private final HashMap<String, String> huffmanTree;
+    private final StringBuilder fileString;
+    private StringBuilder tempString;
+    private HashMap<String, Integer> freqData;
+    private final int nBytesData;
+    private BitSet dataInBits;
 
-    public Huffman(HashMap<String, Integer> receivedData) {
-        if (receivedData.size() > 0) {
-            this.huffmanQueue = new PriorityQueue<>(receivedData.size(), new FrequencyComparator());
-            insertDataIntoPriorityQueue(receivedData);
-            this.huffmanTree = new HashMap<>(receivedData.size());
+    public Huffman(String filePath, int nBytesData) throws IOException {
+        fileString = new StringBuilder(input(filePath));
+        this.nBytesData = nBytesData;
+        setFrequencyMap();
+
+        if (freqData.size() > 0) {
+            this.huffmanQueue = new PriorityQueue<>(freqData.size(), new FrequencyComparator());
+            this.huffmanTree = new HashMap<>(freqData.size());
         } else {
-            throw new IllegalArgumentException("There is no data: " + receivedData.size());
-        }
-    }
-//    public void buildHashMap(String text){
-//        if(text == null || text.length() == 0) return;
-//        Map<Character, Integer> freq = new HashMap<>();
-//        for (char c: text.toCharArray()) {
-//            freq.put(c, freq.getOrDefault(c, 0) + 1);
-//        }
-//        PriorityQueue<FrequencyNode> pq;
-//        pq = new PriorityQueue<>(Comparator.comparingInt(l -> l.freq));
-//
-//        insertDataIntoPriorityQueue(freq);
-//
-//
-//        for (var entry: freq.entrySet()) {
-//            pq.add(new FrequencyNode(entry.getKey(), entry.getValue()));
-//        }
-//
-//    }
-
-   // public Huffman(String text){
-
-   // }
-
-    private void insertDataIntoPriorityQueue(HashMap<String, Integer> data) {
-        for (Map.Entry<String, Integer> freqData : data.entrySet()) {
-            FrequencyNode extractData = new FrequencyNode(freqData.getKey(), freqData.getValue());
-            this.huffmanQueue.add(extractData);
+            throw new IllegalArgumentException("There is no data: " + freqData.size());
         }
     }
 
-    public HashMap<String, String> compressData() {
+    private static StringBuilder input(String filePath) throws IOException {
+        BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(filePath));
+
+        String line;
+        StringBuilder fileData = new StringBuilder();
+
+        while((line = bufferedReader.readLine()) != null)  {
+            fileData.append(line);
+            fileData.append(System.lineSeparator());
+        }
+
+        bufferedReader.close();
+        return fileData;
+    }
+
+    private void setFrequencyMap() {
+        tempString = new StringBuilder();
+        freqData = new HashMap<>();
+
+        for (int endIndex = 0; endIndex < this.fileString.length(); ++endIndex) {
+            appendnBytes(endIndex);
+            updateDataFrequency(tempString.toString());
+            tempString.setLength(0);
+        }
+    }
+
+    private void appendnBytes(int endIndex) {
+        for (int startIndex = endIndex; (startIndex < endIndex + this.nBytesData); ++startIndex) {
+            if ((startIndex < this.fileString.length())) {
+                tempString.append(this.fileString.charAt(startIndex));
+            }
+        }
+    }
+
+    private void updateDataFrequency(String data) {
+        if (freqData.containsKey(data)) {
+            freqData.replace(data, freqData.get(data), freqData.get(data) + 1);
+        } else {
+            freqData.put(data, 1);
+        }
+    }
+
+    public String compressedFile() {
+        compressData();
+        // TODO: Writing to new file.
+        return "";
+    }
+
+    private void compressData() {
+        setHuffmanTree();
+
+        StringBuilder data = new StringBuilder();
+        for (int endIndex = 0; endIndex < fileString.length(); ++endIndex) {
+            appendnBytes(endIndex);
+            data.append(huffmanTree.get(tempString.toString()));
+            tempString.setLength(0);
+        }
+
+        convertDataToBits(data);
+    }
+
+    private void convertDataToBits(StringBuilder data) {
+        dataInBits = new BitSet(data.length());
+        for (int index = 0; index < data.length(); index++) {
+            if (data.charAt(index) == '1') {
+                dataInBits.set(index);
+            }
+        }
+    }
+
+    private void setHuffmanTree() {
+        insertDataIntoPriorityQueue();
         buildHuffmanPriorityQueue();
         buildHuffmanTree(huffmanQueue.peek(), new StringBuilder());
-        return huffmanTree;
+    }
+
+    private void insertDataIntoPriorityQueue() {
+        int index = 0;
+        FrequencyNode[] extractData = new FrequencyNode[freqData.size()];
+        for (Map.Entry<String, Integer> data : freqData.entrySet()) {
+            extractData[index].setnBytesWord(data.getKey());
+            extractData[index].setFrequency(data.getValue());
+            this.huffmanQueue.add(extractData[index++]);
+        }
     }
 
     private void buildHuffmanPriorityQueue() {
@@ -79,6 +143,8 @@ public class Huffman {
         }
     }
 
+
+
     private boolean nullChildren(FrequencyNode root) {
         return (root.left == null && root.right == null);
     }
@@ -101,5 +167,27 @@ public class Huffman {
 //            return index;
 //        }
 //    }
+
+    //    public void buildHashMap(String text){
+//        if(text == null || text.length() == 0) return;
+//        Map<Character, Integer> freq = new HashMap<>();
+//        for (char c: text.toCharArray()) {
+//            freq.put(c, freq.getOrDefault(c, 0) + 1);
+//        }
+//        PriorityQueue<FrequencyNode> pq;
+//        pq = new PriorityQueue<>(Comparator.comparingInt(l -> l.freq));
+//
+//        insertDataIntoPriorityQueue(freq);
+//
+//
+//        for (var entry: freq.entrySet()) {
+//            pq.add(new FrequencyNode(entry.getKey(), entry.getValue()));
+//        }
+//
+//    }
+
+    // public Huffman(String text){
+
+    // }
 
 }
--- part2_6149/src/HuffmanAlgorithm.java
+++ part2_6149/src/HuffmanAlgorithm.java
@@ -1,97 +1,14 @@
-import java.io.*;
-import java.util.BitSet;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 
 public class HuffmanAlgorithm {
 
     public static void main(String[] args) throws IOException {
+        String path = "/Users/mody/Downloads/6060-matrix.cpp";
+        int nWords = 1;
 
-        // Test Huffman Compression.
-        // Assuming the received input is HashMap due to its speed.
-
-//        HashMap<String, Integer> testData = new HashMap<>(10);
-//        testData.put("A", 10);
-//        testData.put("B", 11);
-//        testData.put("C", 5);
-//        testData.put("D", 13);
-//        testData.put("E", 20);
-//        String text = "blah";
-//
-
-
-//
-//        for (Map.Entry<String, String> huffmanData : compressedData.entrySet()) {
-//
-//            System.out.println(huffmanData.getKey() + ": " + huffmanData.getValue());
-//        }
-
-
-            StringBuilder fileString = null;
-            fileString = new StringBuilder(HuffmanAlgorithm.input("/Users/mody/Downloads/DML.txt"));
-
-            System.out.println(fileString);
-            StringBuilder str = new StringBuilder();
-            HashMap<String, Integer> data = new HashMap<>();
-        for (int index = 0; index < fileString.length(); ++index) {
-            for (int inner_index = index; inner_index < index + 1 && inner_index < fileString.length(); ++inner_index) {
-                str.append(fileString.charAt(inner_index));
-            }
-            if (data.containsKey(str.toString())) {
-                data.replace(str.toString(), data.get(str.toString()), data.get(str.toString()) + 1);
-            } else {
-                data.put(str.toString(), 1);
-            }
-            str.setLength(0);
-        }
-
-        for (Map.Entry<String, Integer> freqData : data.entrySet()) {
-            System.out.println(freqData.getKey() + ": " + freqData.getValue());
-        }
-
-        Huffman huffman = new Huffman(data);
-        HashMap<String, String> compressedData = huffman.compressData();
-        for (Map.Entry<String, String> freqData : compressedData.entrySet()) {
-            System.out.println(freqData.getKey() + ": " + freqData.getValue());
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int index = 0; index < fileString.length(); ++index) {
-            for (int inner_index = index; inner_index < index + 1 && inner_index < fileString.length(); ++inner_index) {
-                str.append(fileString.charAt(inner_index));
-            }
-            stringBuilder.append(compressedData.get(str.toString()));
-            str.setLength(0);
-        }
-        System.out.println(stringBuilder.length());
-        BitSet bitSet = new BitSet();
-        for (int index = 0; index < stringBuilder.length(); index++) {
-            if (stringBuilder.charAt(index) == '1') {
-                bitSet.set(index);
-            }
-        }
-
-        System.out.println(bitSet.length());
+        Huffman huffman = new Huffman(path, nWords);
     }
 
-    public static String input(String path) throws IOException {
-        InputStream inputStream = new FileInputStream(path);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-        String fileString;
-        String totalStr = "";
-        while((fileString = bufferedReader.readLine()) != null)
-        {
-            totalStr = totalStr + '\n' + fileString;
-
-        }
-
-        inputStream.close();
-        bufferedReader.close();
-
-        return totalStr;
-
-    }
 
 }
